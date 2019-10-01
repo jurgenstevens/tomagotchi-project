ea070d903c1c0f166944fc346d6e1f5cbd655198
Jurgen Stevens <southsidejdot@Jurgens-MacBook-Air.local>
2019-10-01 15:41:50 -0500

    added buttons and set up functions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
 console.log("Tomagotchi project");
+
+class Tomagotchi {
+
+
+};
+
+
+const game = {
+// Time causes it to change. Stays within the game
+// Create the functions (giveFood, flipTheSwitch, playWith) 
+	giveFood(){
+
+	}
+
+	flipTheSwitch(){
+
+	}
+	playWith(){
+
+	}
+
+
+// and connnect
+
+
+
+};
+
+// Listeners
+$().on(click.) => {}
+$().on(click.) => {}
+$().on(click.) => {}
