d96207ace58b92fed9f34d90d5b0b3499c1015ae
Jurgen Stevens <southsidejdot@Jurgens-MacBook-Air.local>
2019-10-02 14:37:44 -0500

    TamaName was instantiated

--- js/app.js
+++ js/app.js
@@ -1,34 +1,52 @@
 console.log("Tamagotchi project");
 
 class Tamagotchi {
-	constructor(name, age, hungry, bored){
+	constructor(name) {
 		this.legs = 2;
 		this.arms = 2;
 		this.name = name;
-		this.eyes = eyes;
-		this.hungry = null;
-		this.bored = null;
+		this.hungry = 0;
+		this.bored = 0;
+		this.age = 0;
+		// this.eyes = eyes;
 	}
 	
-	greet(tamagotchiName){
-		console.log('Hello! I\'m ' + tamagotchiName + '!');
-	}
+	// greet(tamagotchiName){
+	// 	console.log('Hello! I\'m ' + tamagotchiName + '!');
+	// }
 
-	putToSleep(closeeyes){
-		this.eyes = closeeyes;
-		console.log('Yawn...');
-	}
-};
+	// putToSleep(closeeyes){
+	// 	this.eyes = closeeyes;
+	// 	console.log('Yawn...');
+	// }
+
+}
+
 
 
 const game = {
+	makeTamagotchi(tamaName) {
+		const tamagotchi = new Tamagotchi(tamaName);
+			console.log('Hello! My name is ' + tamaName + '!');
+			setInterval((){
+
+		})
+		// instantiate the Tama class, pass in tamaName
+		// so that your instance will ahve that name
+		// console.log the instantiated object to check
+		}
+	}
+
+
+
+
 // Time causes it to change. Stays within the game
 // Create the functions (giveFood, flipTheSwitch, playWith) 
 	// setInterval(() { 
 	// 	console.log("Hello"); 
 	// }, 30000);
 
-	// giveFood(){
+	// feedTamagatchi(){
 
 	// },
 
@@ -42,14 +60,34 @@ const game = {
 
 
 
-};
+// };
 
 // Listeners
 $('form').on('submit', (e) => {
-	console.log('Clicked! Woo!');
-	console.log( $('#input-box').val());
 	e.preventDefault();
+	console.log('Clicked! Woo!');
+	console.log( $('#input-box').val() );
+	const tamaName = $('#input-box').val();
+	game.makeTamagotchi(tamaName)
+	// call the make tamagatchi method in the game object
+	// pass in tamaName as a parameter
+
 });
+
+// $('#start').on('click', (e) => {
+// 	console.log('Let\'s begin.');
+// 	e.preventDefault();
+// });
+
 // $().on(click.) => {}
 // $().on(click.) => {}
 // $().on(click.) => {}
+
+
+
+
+
+
+
+
+
